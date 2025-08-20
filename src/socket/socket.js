885f4bc0c891d2de9ea3dc58a18f4e4c06a41470
Ravi Kumar Jha <jhakrravi20@gmail.com>
2025-08-20 11:32:14 +0530

    deployment changes

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,19 +1,22 @@
 // src/sockets/index.js
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.VITE_API_URLS;
+const BASE_URL = import.meta.env.PROD
+  ? "https://match-up-arena-server-1.onrender.com"
+  : "http://localhost:3000";
 
 let socket = null;
 
 export const connectSocket = (userId) => {
-  if (!userId || (socket && socket.connected)) return;
+  if (!userId || (socket && socket.connected)) return socket;
 
   socket = io(BASE_URL, {
+    path: "/socket.io",
+    transports: ["websocket"], // ⭐ no polling
     query: { userId },
-    withCredentials: true, // 👈 allow cookies if you’re using them
-    // transports: ["websocket"], // 👈 force websockets (avoid long polling on Render)
-    reconnection: true, // 👈 optional: auto-reconnect
-    reconnectionAttempts: 5, // 👈 retry a few times
+    withCredentials: true,
+    reconnection: true,
+    reconnectionAttempts: 5,
   });
 
   return socket;
@@ -22,8 +25,6 @@ export const connectSocket = (userId) => {
 export const getSocket = () => socket;
 
 export const disconnectSocket = () => {
-  if (socket?.connected) {
-    socket.disconnect();
-    socket = null;
-  }
+  socket?.disconnect();
+  socket = null;
 };
