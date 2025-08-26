7f6cde55aeaa410f477395579b56a24a56e93e39
Ravi Kumar Jha <jhakrravi20@gmail.com>
2025-08-26 21:01:00 +0530

    changed to avatars.jsx

--- src/assets/Avatars.jsx
+++ src/assets/Avatars.jsx
@@ -1,5 +1,5 @@
 // Avatars.jsx
-const Avatars = import.meta.glob("./avatars/*.png", { eager: true });
-
+// const Avatars = import.meta.glob("./avatars/*.png", { eager: true });
+const Avatars = import.meta.glob("./*.png", { eager: true });
 // Convert objects into arrays of image paths
 export const avatars = Object.values(Avatars).map((img) => img.default);
